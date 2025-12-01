a71b37466531eae2ae1a0b2e0b0da9babc7351d4
alexjr <alexjr20041@gmail.com>
2025-11-30 21:05:06 -0300

    funcao botaopesquisar refatorada

--- Classes_Functions.py
+++ Classes_Functions.py
@@ -1,3 +1,6 @@
+import requests
+import tkinter as tk
+
 class Dados:
     def __init__(self, dados_json):
         # Clima
@@ -14,5 +17,49 @@ class Dados:
         self.umidade = dados_json["main"]["humidity"]
 
 
+def botaopesquisar(root,barradebusca):
 
+    # ==== buscar cordenadas ====
+
+    cidadenome= barradebusca.get()
+
+    urlgeo = (
+        f"https://api.openweathermap.org/geo/1.0/direct?"
+        f"q={cidadenome}&limit=1&appid=19e5b3f3a7ba81f1e8734ada48104bcc"
+    )
+
+    respostageo = requests.get(urlgeo)
+    cidade = respostageo.json()
+
+    if len(cidade) == 0:
+        print("Cidade não encontrada")
+        return
+
+    lat = cidade[0]["lat"]
+    lon = cidade[0]["lon"]
+
+    # ==== buscar clima ====
+
+    url_clima = (
+        f"https://api.openweathermap.org/data/2.5/weather?"
+        f"lat={lat}&lon={lon}&appid=19e5b3f3a7ba81f1e8734ada48104bcc"
+    )
+
+    resposta = requests.get(url_clima)
+    dados = Dados(resposta.json())
+
+    # ==== exibir ====
+
+    condicaoresquestlabel = tk.Label(root, text=dados.condicao, font=("Arial", 11), bg='#17517E', fg='#FFFFFF')
+    condicaoresquestlabel.place(x=200, y=200)
+
+    descricaoresquestlabel = tk.Label(root, text=dados.descricao, font=("Arial", 11), bg='#17517E', fg='#FFFFFF')
+    descricaoresquestlabel.place(x=225, y=230)
+
+    temperaturarequestlabel = tk.Label(root, text=f"{dados.temperatura}°C", font=("Arial", 11), bg='#17517E', fg='#FFFFFF')
+    temperaturarequestlabel.place(x=250, y=260)
+
+
+    umidaderequestlabel = tk.Label(root, text=f"{dados.umidade}%", font=("Arial", 11), bg='#17517E', fg='#FFFFFF')
+    umidaderequestlabel.place(x=225, y=290)
 
--- Main.py
+++ Main.py
@@ -1,52 +1,5 @@
-import requests
 import tkinter as tk
 from Classes_Functions import *
-
-def botaopesquisar():
-
-    # ==== buscar cordenadas ====
-
-    cidadenome= barradebusca.get()
-
-    urlgeo = (
-        f"https://api.openweathermap.org/geo/1.0/direct?"
-        f"q={cidadenome}&limit=1&appid=19e5b3f3a7ba81f1e8734ada48104bcc"
-    )
-
-    respostageo = requests.get(urlgeo)
-    cidade = respostageo.json()
-
-    if len(cidade) == 0:
-        print("Cidade não encontrada")
-        return
-
-    lat = cidade[0]["lat"]
-    lon = cidade[0]["lon"]
-
-    # ==== buscar clima ====
-
-    url_clima = (
-        f"https://api.openweathermap.org/data/2.5/weather?"
-        f"lat={lat}&lon={lon}&appid=19e5b3f3a7ba81f1e8734ada48104bcc"
-    )
-
-    resposta = requests.get(url_clima)
-    dados = Dados(resposta.json())
-
-    # ==== exibir ====
-
-    condicaoresquestlabel = tk.Label(root, text=dados.condicao, font=("Arial", 11), bg='#17517E', fg='#FFFFFF')
-    condicaoresquestlabel.place(x=200, y=200)
-
-    descricaoresquestlabel = tk.Label(root, text=dados.descricao, font=("Arial", 11), bg='#17517E', fg='#FFFFFF')
-    descricaoresquestlabel.place(x=225, y=230)
-
-    temperaturarequestlabel = tk.Label(root, text=f"{dados.temperatura}°C", font=("Arial", 11), bg='#17517E', fg='#FFFFFF')
-    temperaturarequestlabel.place(x=250, y=260)
-
-
-    umidaderequestlabel = tk.Label(root, text=f"{dados.umidade}%", font=("Arial", 11), bg='#17517E', fg='#FFFFFF')
-    umidaderequestlabel.place(x=225, y=290)
 
 
 # ========= Instanciar Janela =========
@@ -64,8 +17,8 @@ barradebusca.place(x=120, y=50)
 
 
 # ===== Botão =====
-botaopesquisar = tk.Button(root, text='Buscar',command=botaopesquisar,bd=0)
-botaopesquisar.place(x=70, y=50)
+botaopesquisa = tk.Button(root, text='Buscar',command=lambda: botaopesquisar(root,barradebusca),bd=0)
+botaopesquisa.place(x=70, y=50)
 
 
 # ===== Labels =====
